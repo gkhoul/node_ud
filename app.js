4a2d47aaf24f7c04efa4e49bbcc92f51d35a2cb1
gkhoul <gkhoul@yandex.ru>
2016-08-29 17:44:10 +0500

    Fixed package.json

---
+++ app.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    var http = require('http');
+    var fs = require('fs');
+
+    http.createServer(function (req, res) {
+
+        if (req.url === '/'){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.createReadStream(__dirname + '/index.html', 'utf8').pipe(res);
+        }
+
+        else if (req.url === '/api'){
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            var obj = {
+                firstname: 'John', lastname: 'Doe'
+            };
+            res.end(JSON.stringify(obj));
+        }
+        else {
+            res.writeHead(404);
+            res.end();
+        }
+
+    }).listen(1337, '0.0.0.0')
+
+}());
